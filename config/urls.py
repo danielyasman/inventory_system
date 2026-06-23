90bbdf78649c5806d9b9228a9116c39b4192a9d0
danielyasman <daniel.yasman1998@gmail.com>
2026-06-23 19:11:05 +0300

    feat: add about and contact pages

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from pages.views import home
+from pages.views import home, about, contact
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',home)
+    path('',home),
+    path('about/',about),
+    path('contact/',contact),
 ]
